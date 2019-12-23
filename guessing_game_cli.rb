a5bfbc85eaa63a03d54720f03787e5531b500c23
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-23 03:12:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,19 +1,3 @@
-# def run_guessing_game
-# randon_number = rand(1..6)
-# puts 'Hello there, Can you tell me the number from 1 to 6?'
-# user_num = gets.to_i
-# if randon_number = user_num
-#   puts "/You guessed the correct number!/"
-# end 
-# if randon_number!= user_num 
-#   puts "Sorry! The computer guessed #{randon_number}."
-# end
-# if exit
-#   puts "Goodbye!"
-# end
-# end
-
-
 def run_guessing_game
 randon_number = rand(1..6)
 puts 'Hello there, Can you tell me the number from 1 to 6?'
@@ -21,9 +5,11 @@ user_num = gets.to_i
 if randon_number = user_num
   puts "/You guessed the correct number!/"
 end 
-elsif user_num = "exit"
+if randon_number!= user_num 
+  puts "Sorry! The computer guessed #{randon_number}."
+end
+if exit
   puts "Goodbye!"
 end
-else "Sorry! The computer guessed #{randon_number}."
 end
-end
+
