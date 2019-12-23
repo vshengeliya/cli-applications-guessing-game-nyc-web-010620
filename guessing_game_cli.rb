97ea1c49e66f982fa67d655a2efb5646d0c297b6
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-23 02:45:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,8 @@
 def run_guessing_game
 randon_number = rand(1..6)
-number = gets.chomp.to_i
-puts "Please enter a number from 1 to 6 "
+puts 'Hello there, Can you tell me your the number from 1 to 6?'
+user_num = gets.to_i
+# puts 'Your favourite number is ' + num.to_s + '?'
+if randon_number = user_num
+  
 end
